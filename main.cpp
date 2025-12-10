9cb8ef57a600e4a3a33c76ee39197c27a98c8b07
wdemirkoll <wdemirkoll@gmail.com>
2025-12-10 21:44:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int menuProcess;
 int number[2];
 
+// Displays the main menu and reads user choice
 void mainMenu(){
     std::cout << "[1] - Addition Process" << std::endl;
     std::cout << "[2] - Subtraction Process" << std::endl;
@@ -12,6 +13,7 @@ void mainMenu(){
     std::cin >> menuProcess;
 }
 
+// Gets two numbers from the user
 int NumbersInput(){
     std::cout << "Enter first number:";
     std::cin >> number[0];
@@ -22,49 +24,51 @@ int NumbersInput(){
     return 0;
 }
 
+// Adds the two numbers and prints the result
 void AdditionProcess(){
     std::cout << "Sum of numbers:" << number[0] + number[1] << std::endl;
 }
 
+// Subtracts the second number from the first
 void SubtractionProcess(){
     std::cout << "Difference of number:" << number[0] - number[1] << std::endl;
 }
 
+// Multiplies the two numbers
 void MultiplicationProcess(){
     std::cout << "Multiplication of numbers:" << number[0] * number[1] << std::endl;
 }
 
+// Divides the first number by the second
 void DivisionProcess(){
     std::cout << "Division of number:" << number[0] / number[1] << std::endl;
 }
 
 int main(){
+    // Display the menu for operation selection
     mainMenu();
 
+    // Execute the operation chosen by the user
     switch(menuProcess){
     case 1:
         NumbersInput();
         AdditionProcess();
         break;
 
-
     case 2:
         NumbersInput();
         SubtractionProcess();
         break;
-
 
     case 3:
         NumbersInput();
         MultiplicationProcess();
         break;
 
-
     case 4:
         NumbersInput();
         DivisionProcess();
         break;
-
 
     default:
         std::cout << "Invalid selection!" << std::endl;
